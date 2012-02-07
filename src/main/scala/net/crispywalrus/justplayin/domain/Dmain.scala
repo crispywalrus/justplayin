7edbab921cf8ffeb9c3aa76c3f74d5d7da8ca98d
crispy <crispywalrus@gmail.com>
2012-02-07 09:35:56 -0800

    start wiring up a domain model to mysql using squeryl

---
+++ src/main/scala/net/crispywalrus/justplayin/domain/Dmain.scala
@@ -0,0 +1,40 @@
+package net.crispywalrus.justplayin.domain
+
+import org.squeryl.PrimitiveTypeMode._
+import org.squeryl.Session
+import org.squeryl.SessionFactory
+import org.squeryl.adapters._
+import org.squeryl.internals.DatabaseAdapter
+import net.crispywalrus.justplayin.domain._
+
+
+object Dmain {
+
+  def main(args: Array[String]) {
+    createDbSession("jdbc:mysql://localhost:3306/justplayindb?user=liqui&password=base",new MySQLInnoDBAdapter)
+
+    // transaction {
+    //   schema.create
+    // }
+
+    inTransaction {
+      try {
+        val user:User = new User("crispywalrus@gmail.com","crispy")
+        schema.users.insert(user)
+      } catch {
+        case e:RuntimeException => {
+          println("bogosity: "+e.getCause())
+          println("bogosity: "+e.getMessage())
+        }
+      }
+    }
+    
+    println("done "+readLine())
+  }
+
+  def createDbSession(dbUrl:String,adapter:DatabaseAdapter): Unit = {
+    Class.forName("com.mysql.jdbc.Driver")
+    SessionFactory.concreteFactory = Some(() => Session.create(java.sql.DriverManager.getConnection(dbUrl),adapter))
+  }
+
+}
--- src/main/scala/net/crispywalrus/justplayin/domain/Domain.scala
+++ src/main/scala/net/crispywalrus/justplayin/domain/Domain.scala
@@ -5,10 +5,25 @@ import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.Schema
 import org.squeryl.annotations.Column
 
-case class User(id:Long,firstName:String,lastName:String,handle:String,email:String) extends KeyedEntity[Long]
+class DomainModel extends KeyedEntity[Long] {
+  val id: Long = 0
+  @Column("created_at") 
+  val createdAt = new java.sql.Timestamp(System.currentTimeMillis) 
+  @Column("updated_at") 
+  var updatedAt = new java.sql.Timestamp(System.currentTimeMillis)
+}
+
+case class User(firstName:String,lastName:String,handle:String,email:String) extends DomainModel {
+  def this(email:String,handle:String) = this("","",handle,email)
+}
 
 object schema extends Schema {
 
   val users = table[User]
 
+  on(users)(user => declare(
+    user.id is (autoIncremented),
+    user.email is (unique),
+    user.handle is (unique)
+  ))
 }
